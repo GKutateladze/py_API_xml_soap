287e3ac9b7d838f15c2e7877cdb9790224e281f9
GeorgKutateladze <georgerailz@gmail.com>
2018-03-27 22:07:11 +0300

    1) Temperatures + 2) Currencies   + 3) Distances +

--- py_soap_hw.py
+++ py_soap_hw.py
@@ -4,27 +4,11 @@ import pprint
 from xml.etree.ElementTree import fromstring
 
 
-# ------------------- PART 1 -------------------
-def temperature(degreeFahrenheit):
-    response = requests.post(
-        'http://www.webservicex.net/ConvertTemperature.asmx',
-        headers={
-            'Content-Type': 'text/xml; charset=utf-8'
-        },
-        data='''<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <ConvertTemp xmlns="http://www.webserviceX.NET/">
-      <Temperature>''' + str(degreeFahrenheit) + '''</Temperature>
-      <FromUnit>degreeFahrenheit</FromUnit>
-      <ToUnit>degreeCelsius</ToUnit>
-    </ConvertTemp>
-  </soap:Body>
-</soap:Envelope>'''
-    )
+#------------------- PART 1 -------------------
+client = osa.Client('http://www.webservicex.net/ConvertTemperature.asmx?WSDL')
 
-    return (fromstring(response.text)[0][0][0].text)
-
+def temperature(temp, fromUnit, toUnit):
+    return client.service.ConvertTemp(temp, fromUnit, toUnit)
 
 def average_temperature(text):
     with open(text) as f:
@@ -33,13 +17,13 @@ def average_temperature(text):
             temp = int(line.strip()[0:2])
             # print(f'{temp}°F is {(temperature(temp))[0:(len(".")+2)]}°C')
             temperatures.append(temp)
-        print(f'Average temperature is {temperature(sum(temperatures)/len(temperatures))[0:4]}°C')
+        print(f'Average temperature is {round(int(float(temperature(sum(temperatures)/len(temperatures), "degreeFahrenheit", "degreeCelsius"))))}°C')
 
 average_temperature('temps.txt')
 print('-----------------------------')
 
 
-# ------------------- PART 2 -------------------
+#------------------- PART 2 -------------------
 def currencies(amount, fromCurrency):
     response = requests.post(
         'http://fx.currencysystem.com/webservices/CurrencyServer4.asmx',
@@ -65,6 +49,33 @@ def currencies(amount, fromCurrency):
     return (fromstring(response.text)[0][0][0].text)
 
 with open('currencies.txt') as f:
+    total_flight = []
     for line in f:
         flight = (line.strip()).split(' ')
-        print(flight[0], currencies(flight[1], flight[2]), 'RUB')
+        #print(flight[0], round(int(float(currencies(flight[1], flight[2])))), 'RUB')
+        total_flight.append(int(float(currencies(flight[1], flight[2]))))
+    print(f'TOTAL cost: {sum(total_flight)} RUB')
+    print('-----------------------------')
+
+
+#------------------- PART 3 -------------------
+client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
+
+def distance(distance, frm, to):
+    return client.service.ChangeLengthUnit(distance, frm, to)
+
+
+with open('travel.txt') as f:
+    split_list = []
+    for line in f:
+        lenght = (line.strip().split(' '))
+        split_list.append(lenght[1])
+    real_numbers = []
+    for item in split_list:
+        if ',' in item:
+            split = item.split(',')
+            real_numbers.append(int(float(split[0]+split[1])))
+        elif ',' not in item:
+            real_numbers.append(int(float(item)))
+
+    print(f'TOTAL distance: {(round((distance(sum(real_numbers), "Miles", "Kilometers")),2))} KM')
